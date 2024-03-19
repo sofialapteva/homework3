71f721988093fa65a5233595c6a91f2f33b97106
Sofia Lapteva <sofia_lapteva@mail.ru>
2024-03-19 18:06:32 +0300

    init

---
+++ lib/homework3.ex
@@ -0,0 +1,18 @@
+defmodule Homework3 do
+  @moduledoc """
+  Documentation for `Homework3`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Homework3.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/homework3_test.exs
@@ -0,0 +1,8 @@
+defmodule Homework3Test do
+  use ExUnit.Case
+  doctest Homework3
+
+  test "greets the world" do
+    assert Homework3.hello() == :world
+  end
+end
